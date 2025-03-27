d23870178940831be03ff761010b3463ebe101d7
Andrew Consroe <aconz2@gmail.com>
2025-03-27 13:39:21 -0500

    was truncating the rng_u64

--- table.c
+++ table.c
@@ -28,11 +28,16 @@ static u64 elapsed_ns(Timespec start, Timespec stop) {
 
 // https://arxiv.org/pdf/1504.06804
 // hashes x universally into l<=64 bits using random odd seed a.
-u64 hash1(u64 x, u64 l, u64 a) {
-    a = 0x9e3779b97f4a7c15;
-    return (a*x) >> (64-l);
+u64 hash1(u64 x, u64 l, u64 H[2]) {
+    u64 a = H[0];
+    u64 b = H[1];
+    /*u64 a = 0x9e3779b97f4a7c15;*/
+    /*return (a*x) >> (64-l);*/
+    /*return (a*x+b) >> (64-l);*/
+    /*return ((x + b) * a) >> (64-l);*/
+    /*return (x * a) >> (64-l);*/
     /*return _bzhi_u64(a * x, l);*/
-    /*return (a*x) & ((1 << l) - 1);*/
+    return (a*x) & ((1 << l) - 1);
     /*(void)a;*/
     // multiplying by p turns into a shl and sub
     /*u64 p = 0xffffffffffff;*/
@@ -45,6 +50,36 @@ u64 hash1(u64 x, u64 l, u64 a) {
 /*static u64 hash1(u32 x, u64 l, u64 a) {*/
     /*return _mm_crc32_u32((u32)a, x) >> (32-l);*/
     /*return _mm_crc32_u32((u32)a, x) & ((1 << l) - 1);*/
+/*}*/
+
+// murmur
+/*u64 hash1(u64 x, u64 l, u64 a) {*/
+/*  x ^= x >> 33;*/
+/*  x *= 0xff51afd7ed558ccdL;*/
+/*  x ^= x >> 33;*/
+/*  x *= 0xc4ceb9fe1a85ec53L;*/
+/*  x ^= x >> 33;*/
+/*  return x >> (64 - l);*/
+/*}*/
+
+// https://github.com/skeeto/hash-prospector
+/*u32 hash1(u32 x, u64 l, u64 H[2]) {*/
+/*    x ^= x >> 17;*/
+/*    x *= 0xed5ad4bb;*/
+/*    x ^= x >> 11;*/
+/*    x *= 0xac4c1b51;*/
+/*    x ^= x >> 15;*/
+/*    x *= 0x31848bab;*/
+/*    x ^= x >> 14;*/
+/*    return x >> (32 - l);*/
+/*}*/
+/*u32 hash1(u32 x, u64 l, u64 H[2]) {*/
+/*    x ^= x >> 16;*/
+/*    x *= 0x7feb352d;*/
+/*    x ^= x >> 15;*/
+/*    x *= 0x846ca68b;*/
+/*    x ^= x >> 16;*/
+/*    return x >> (32-l);*/
 /*}*/
 
 void rng_init(PRNG32RomuQuad* rng, u8 seed) {
@@ -60,7 +95,7 @@ void rng_init(PRNG32RomuQuad* rng, u8 seed) {
 }
 
 u32 rng_u32(PRNG32RomuQuad* rng) { return prng32_romu_quad(rng); }
-u32 rng_u64(PRNG32RomuQuad* rng) {
+u64 rng_u64(PRNG32RomuQuad* rng) {
     u64 a = rng_u32(rng);
     u64 b = rng_u32(rng);
     return (a << 32) | b;
@@ -79,12 +114,28 @@ u16 ymm2_entry_cmp(__m256i v[2], u32 key) {
 
 // ----------------------------------- Table1 -------------------------------
 
+// by having 64 bit values, we annoyingly take up 32 + 64 bytes for each bucket
+// this makes the index calculation one longer
+//
+// 32 bit values
+//  40161c: c4 e2 cb f7 c2               	shrx	rax, rdx, rsi
+//  401621: 48 c1 e0 06                  	shl	rax, 0x6
+//  401625: c5 fd 76 04 07               	vpcmpeqd	ymm0, ymm0, ymmword ptr [rdi + rax]
+//
+// 64 bit values
+//  40162c: c4 e2 cb f7 c2               	shrx	rax, rdx, rsi
+//  401631: 48 8d 04 40                  	lea	rax, [rax + 2*rax]
+//  401635: 48 c1 e0 05                  	shl	rax, 0x5
+//  401639: c5 fd 76 04 07               	vpcmpeqd	ymm0, ymm0, ymmword ptr [rdi + rax]
+//
+//  we also get about 1.49ns/lookup vs 1.14 ns/lookup with 64 vs 32 bit value
+//  1.30 vs 0.96 for batch 4
 typedef struct {
     union {
         __m256i kv; // (k)ey (v)ector
         u32 key[8];
     };
-    u64 value[8];
+    u32 value[8];
 } Table1Bucket; // 1.5 cache lines
 
 size_t Table1_n_buckets(size_t n_bits) { return ((u64)1) << n_bits; }
@@ -116,7 +167,7 @@ void Table1_reset(Table1Bucket* table, size_t n_bits) {
     }
 }
 
-int Table1_insert(Table1Bucket* table, size_t n_bits, u64 H, u32 key, u64 value) {
+int Table1_insert(Table1Bucket* table, size_t n_bits, u64 H[2], u32 key, u64 value) {
     size_t bucket = hash1(key, n_bits, H);
     u8 mask = ymm_entry_cmp(table[bucket].kv, key);
     if (mask != 0) { return 1; } // key existed
@@ -129,7 +180,7 @@ int Table1_insert(Table1Bucket* table, size_t n_bits, u64 H, u32 key, u64 value)
     return 0;
 }
 
-int Table1_get(Table1Bucket* table, size_t n_bits, u64 H, u32 key, u64* value) {
+int Table1_get(Table1Bucket* table, size_t n_bits, u64 H[2], u32 key, u64* value) {
     size_t bucket = hash1(key, n_bits, H);
     u8 mask = ymm_entry_cmp(table[bucket].kv, key);
     if (mask == 0) { return 1; } // key not found
@@ -139,9 +190,11 @@ int Table1_get(Table1Bucket* table, size_t n_bits, u64 H, u32 key, u64* value) {
     return 0;
 }
 
-int Table1_get_batch2(Table1Bucket* table, size_t n_bits, u64 H, u32 key[2], u64 value[2]) {
+int Table1_get_batch2(Table1Bucket* table, size_t n_bits, u64 H[2], u32 key[2], u64 value[2]) {
+/*#define IMPL1*/
+#ifdef IMPL1
     size_t bucket[2];
-    u8 mask[4];
+    u8 mask[2];
     for (size_t i = 0; i < 2; i++) { bucket[i] = hash1(key[i], n_bits, H); }
     for (size_t i = 0; i < 2; i++) { mask[i] = ymm_entry_cmp(table[bucket[i]].kv, key[i]); }
     int ret = 0;
@@ -153,9 +206,33 @@ int Table1_get_batch2(Table1Bucket* table, size_t n_bits, u64 H, u32 key[2], u64
         }
     }
     return ret;
+
+#else
+    // this does worse! I think because we increase the latency to loading the first bucket
+    // since we can do work for the second one while the load for the first happens
+    u8 mask[2];
+    Table1Bucket* bucket0 = table + hash1(key[0], n_bits, H);
+    Table1Bucket* bucket1 = table + hash1(key[1], n_bits, H);
+    __m256 kv0 = _mm256_set1_epi32(key[0]);
+    mask[0] = _mm256_movemask_ps(_mm256_cmpeq_epi32(bucket0->kv, kv0));
+    __m256 kv1 = _mm256_set1_epi32(key[1]);
+    mask[1] = _mm256_movemask_ps(_mm256_cmpeq_epi32(bucket1->kv, kv1));
+    int ret = 0;
+    if (mask[0] == 0) {
+        ret |= 1;
+    } else {
+        value[0] = bucket0->value[__builtin_ctz(mask[0])];
+    }
+    if (mask[1] == 0) {
+        ret |= 2;
+    } else {
+        value[1] = bucket1->value[__builtin_ctz(mask[1])];
+    }
+    return ret;
+#endif
 }
 
-int Table1_get_batch4(Table1Bucket* table, size_t n_bits, u64 H, u32 key[4], u64 value[4]) {
+int Table1_get_batch4(Table1Bucket* table, size_t n_bits, u64 H[2], u32 key[4], u64 value[4]) {
     size_t bucket[4];
     u8 mask[4];
     for (size_t i = 0; i < 4; i++) { bucket[i] = hash1(key[i], n_bits, H); }
@@ -177,7 +254,10 @@ size_t Table1_try_build(Table1Bucket* table, size_t n_bits, u32* keys, size_t ta
     while (try <= tries) {
         try += 1;
         rng_init(&rng, 42 + try);
-        u64 Htry = rng_u64(&rng) * 2 + 1;
+        u64 Htry[2] = {
+            rng_u64(&rng) * 2 + 1,
+            rng_u64(&rng),
+        };
         Table1_reset(table, n_bits);
 
         /*printf("trying H=%ld\n", Htry);*/
@@ -205,7 +285,8 @@ size_t Table1_try_build(Table1Bucket* table, size_t n_bits, u32* keys, size_t ta
             keys[occupancy] = key;
             occupancy += 1;
         }
-        *H = Htry;
+        H[0] = Htry[0];
+        H[1] = Htry[1];
         return try;
 nexttry:
         (void)0;
@@ -274,7 +355,7 @@ void Table2_reset(Table2Bucket* table, size_t n_bits) {
     }
 }
 
-int Table2_insert(Table2Bucket* table, size_t n_bits, u64 H, u32 key, u64 value) {
+int Table2_insert(Table2Bucket* table, size_t n_bits, u64 H[2], u32 key, u64 value) {
     size_t bucket = hash1(key, n_bits, H);
     u8 ma = ymm_entry_cmp(table[bucket].kv[0], key);
     u8 mb = ymm_entry_cmp(table[bucket].kv[1], key);
@@ -290,7 +371,7 @@ int Table2_insert(Table2Bucket* table, size_t n_bits, u64 H, u32 key, u64 value)
     return 0;
 }
 
-int Table2_get(Table2Bucket* table, size_t n_bits, u64 H, u32 key, u64* value) {
+int Table2_get(Table2Bucket* table, size_t n_bits, u64 H[2], u32 key, u64* value) {
     size_t bucket = hash1(key, n_bits, H);
     u16 mask = ymm2_entry_cmp(table[bucket].kv, key);
     if (mask == 0) { return 1; } // key not found
@@ -306,7 +387,10 @@ size_t Table2_try_build(Table2Bucket* table, size_t n_bits, u32* keys, size_t ta
     while (try <= tries) {
         try += 1;
         rng_init(&rng, 42 + try);
-        u64 Htry = rng_u64(&rng) * 2 + 1;
+        u64 Htry[2] = {
+            rng_u64(&rng) * 2 + 1,
+            rng_u64(&rng),
+        };
         Table2_reset(table, n_bits);
 
         /*printf("trying H=%ld\n", Htry);*/
@@ -334,7 +418,8 @@ size_t Table2_try_build(Table2Bucket* table, size_t n_bits, u32* keys, size_t ta
             keys[occupancy] = key;
             occupancy += 1;
         }
-        *H = Htry;
+        H[0] = Htry[0];
+        H[1] = Htry[1];
         return try;
 nexttry:
         (void)0;
@@ -342,7 +427,7 @@ nexttry:
     return 0;
 }
 
-size_t Table2_bsearch_occupancy(Table2Bucket* table, size_t n_bits, u32* keys, size_t tries, u64* H) {
+size_t Table2_bsearch_occupancy(Table2Bucket* table, size_t n_bits, u32* keys, size_t tries, u64 H[2]) {
     size_t size = ((u64)1) << n_bits;
     size_t size_entries = size * 8;
     size_t lo = 1;
@@ -381,14 +466,17 @@ int main() {
     }
 #endif
 
+    printf("alignof(Table1Bucket) = %ld\n", _Alignof(Table1Bucket));
+    printf("alignof(Table2Bucket) = %ld\n", _Alignof(Table2Bucket));
+
 #ifndef NDEBUG
-        const size_t rounds = 1000;
+        const size_t rounds = 2000;
 #else
         const size_t rounds = 10;
 #endif
 
     size_t tries = 10000;
-    size_t target_size = 1ll << 15;
+    size_t target_size = 1ll << 14;
     size_t table_1_bits = __builtin_ctz(target_size / 8);
     size_t table_2_bits = __builtin_ctz(target_size / 16);
     printf("table_1_bits=%ld table_2_bits=%ld\n", table_1_bits, table_2_bits);
@@ -400,12 +488,13 @@ int main() {
         size_t n_entries = Table1_n_entries(n_bits);
         Table1Bucket* table = Table1_create(n_bits);
         u32* keys = calloc(n_entries, sizeof(u32));
-        u64 H = 0;
-        /*size_t try = try_table_build(table, n_bits, keys, target_size, tries, &H);*/
-        size_t got_entries = Table1_bsearch_occupancy(table, n_bits, keys, tries, &H);
+        u64 H[2];
+        /*size_t try = try_table_build(table, n_bits, keys, target_size, tries, H);*/
+        size_t got_entries = Table1_bsearch_occupancy(table, n_bits, keys, tries, H);
         printf("table1 got %ld num_entries\n", got_entries);
         printf("table1 %ld buckets, %ld entries\n", n_buckets, n_entries);
         printf("%.2f occupancy\n", (double)got_entries/(double)n_entries);
+        printf("H = {%lx, %lx}\n", H[0], H[1]);
 
 #ifndef NDEBUG
         for (size_t i = 0; i < got_entries; i++) {
@@ -492,14 +581,14 @@ int main() {
 
     printf("-------------\n");
 
-    {
+    if (0) {
         size_t n_bits = table_2_bits;
         size_t n_buckets = Table2_n_buckets(n_bits);
         size_t n_entries = Table2_n_entries(n_bits);
         Table2Bucket* table = Table2_create(n_bits);
         u32* keys = calloc(n_entries, sizeof(u32));
-        u64 H = 0;
-        size_t got_entries = Table2_bsearch_occupancy(table, n_bits, keys, tries, &H);
+        u64 H[2];
+        size_t got_entries = Table2_bsearch_occupancy(table, n_bits, keys, tries, H);
         printf("table2 got %ld num_entries\n", got_entries);
         printf("table2 %ld buckets, %ld entries\n", n_buckets, n_entries);
         printf("%.2f occupancy\n", (double)got_entries/(double)n_entries);
